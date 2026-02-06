59b917b7c77d7a21b4550e24864e8c58a1ec05ef
Andresxx <andresxbd@gmail.com>
2026-02-06 12:43:53 -0400

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/landing-page-axis/' : '/',
+  base: '/landing-page-axis/',
   server: {
     host: "::",
     port: 8080,
